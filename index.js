7b78580c8579292eeee88adb0fd92c0617a8c6e1
yangga <yangga0070@gmail.com>
2018-03-23 21:07:29 +0900

    added object module

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = exports = {
     fs: require('./lib/fs'),
     mod: require('./lib/mod'),
     number: require('./lib/number'),
+    object: require('./lib/object'),
     string: require('./lib/string'),
 }
